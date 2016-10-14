53795fdf04c9a592247e3fb23329913dd2d7dd55
LadislavSzolik <ladislavszolik@Ladislavs-MacBook-Pro.local>
2016-10-15 00:36:34 +0200

    First prototype

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var ctnApp = angular.module('ctnApp',[]);
+
+ctnApp.controller('dayScheduleCtrl', ['$scope', function($scope){
+
+
+  $scope.tasks = ['Take out the Trash', 'Play a good game', 'Buy a new house'];
+
+}]);
