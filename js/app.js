333407d4fe427dd4f13c60bca235170df0f1a140
Ladislav Szolik <szolik.ladislav@gmail.com>
2016-10-15 16:53:41 +0200

    finito

--- js/app.js
+++ js/app.js
@@ -19,13 +19,13 @@ ctnApp.config(['$routeProvider', function($routeProvider) {
         });
 }]);
 
-ctnApp.controller('loginCtrl', ['$scope', '$window', 'SharedBackend', function($scope, $window,SharedBackend) {
-  $scope.goSearch = function(){
-    $window.location.href = '#/search';
-  };
+ctnApp.controller('loginCtrl', ['$scope', '$window', 'SharedBackend', function($scope, $window, SharedBackend) {
+    $scope.goSearch = function() {
+        $window.location.href = '#/search';
+    };
 }]);
 
-ctnApp.controller('stepCtrl', ['$scope','SharedBackend', function($scope, SharedBackend) {
+ctnApp.controller('stepCtrl', ['$scope', 'SharedBackend', function($scope, SharedBackend) {
 
     $scope.showPrevious = function() {
         for (var i = 0; i < $scope.listOfTasks.length; i++) {
@@ -51,33 +51,37 @@ ctnApp.controller('stepCtrl', ['$scope','SharedBackend', function($scope, Shared
         }
     }
 
-    $scope.setDone = function () {
-      for (var i = 0; i < $scope.listOfTasks.length; i++) {
-          if ($scope.listOfTasks[i].isCurrent == true) {
-            $scope.listOfTasks[i].isDone = true;
-              if ($scope.listOfTasks[i + 1] != null) {
-                  $scope.listOfTasks[i].isCurrent = false;
-                  $scope.listOfTasks[i + 1].isCurrent = true;
-              }else {
-                SharedBackend.listOfHowTo[0].inprogress = false;
-                SharedBackend.listOfHowTo[1].isRecommended = true;
-                SharedBackend.listOfHowTo[2].isRecommended = true;
-                SharedBackend.listOfHowTo[3].isRecommended = false;
-                SharedBackend.listOfHowTo[4].isRecommended = false;
-              }
-              break
-          }
-      }
+    $scope.setDone = function() {
+        for (var i = 0; i < $scope.listOfTasks.length; i++) {
+            if ($scope.listOfTasks[i].isCurrent == true) {
+                $scope.listOfTasks[i].isDone = true;
+                if ($scope.listOfTasks[i + 1] != null) {
+                    $scope.listOfTasks[i].isCurrent = false;
+                    $scope.listOfTasks[i + 1].isCurrent = true;
+                } else {
+                    for (var i = 0; i < SharedBackend.listOfHowTo.length; i++) {
+                        if (SharedBackend.currID == SharedBackend.listOfHowTo[i].ID) {
+                            SharedBackend.listOfHowTo[i].inprogress = false;
+                        }
+                    }
+                    SharedBackend.listOfHowTo[1].isRecommended = true;
+                    SharedBackend.listOfHowTo[2].isRecommended = true;
+                    SharedBackend.listOfHowTo[3].isRecommended = false;
+                    SharedBackend.listOfHowTo[4].isRecommended = false;
+                }
+                break
+            }
+        }
 
 
 
     }
 
-    for(var i=0; i<SharedBackend.listOfHowTo.length; i++) {
-      if(SharedBackend.currID == SharedBackend.listOfHowTo[i].ID) {
-        $scope.listOfTasks = SharedBackend.listOfHowTo[i].listOfTasks;
-        $scope.title = SharedBackend.listOfHowTo[i].title;
-      }
+    for (var i = 0; i < SharedBackend.listOfHowTo.length; i++) {
+        if (SharedBackend.currID == SharedBackend.listOfHowTo[i].ID) {
+            $scope.listOfTasks = SharedBackend.listOfHowTo[i].listOfTasks;
+            $scope.title = SharedBackend.listOfHowTo[i].title;
+        }
     }
 
 }]);
@@ -85,23 +89,23 @@ ctnApp.controller('stepCtrl', ['$scope','SharedBackend', function($scope, Shared
 ctnApp.controller('searchCtrl', ['$scope', '$window', 'SharedBackend', function($scope, $window, SharedBackend) {
     $scope.recommended = [];
     $scope.otherHowTo = [];
-    for(var i=0; i<SharedBackend.listOfHowTo.length; i++) {
-      if(SharedBackend.listOfHowTo[i].isRecommended) {
-        $scope.recommended.push(SharedBackend.listOfHowTo[i]);
-      } else {
-        $scope.otherHowTo.push(SharedBackend.listOfHowTo[i]);
-      }
+    for (var i = 0; i < SharedBackend.listOfHowTo.length; i++) {
+        if (SharedBackend.listOfHowTo[i].isRecommended) {
+            $scope.recommended.push(SharedBackend.listOfHowTo[i]);
+        } else {
+            $scope.otherHowTo.push(SharedBackend.listOfHowTo[i]);
+        }
     }
 
 
-    $scope.goOverview = function(selectedId){
+    $scope.goOverview = function(selectedId) {
 
-      for(var i=0; i< SharedBackend.listOfHowTo.length; i++) {
-        if(selectedId == SharedBackend.listOfHowTo[i].ID) {
-          SharedBackend.currID = 1;
+        for (var i = 0; i < SharedBackend.listOfHowTo.length; i++) {
+            if (selectedId == SharedBackend.listOfHowTo[i].ID) {
+                SharedBackend.currID = selectedId;
+            }
         }
-      }
-      $window.location.href = '#/overview';
+        $window.location.href = '#/overview';
 
 
     };
@@ -109,48 +113,61 @@ ctnApp.controller('searchCtrl', ['$scope', '$window', 'SharedBackend', function(
 
 }]);
 
-ctnApp.controller('myProgressCtrl', ['$scope', function($scope) {
-  $scope.taskInProgress = ['Get a driving license in Switzerland'];
+ctnApp.controller('myProgressCtrl', ['$scope', '$window', 'SharedBackend', function($scope, $window, SharedBackend) {
 
-  $scope.goOverview = function(selectedId){
-
-    for(var i=0; i< SharedBackend.listOfHowTo.length; i++) {
-      if(selectedId == SharedBackend.listOfHowTo[i].ID) {
-        SharedBackend.currID = 1;
-      }
+    $scope.taskInProgress = [];
+    for (var i = 0; i < SharedBackend.listOfHowTo.length; i++) {
+        if (SharedBackend.listOfHowTo[i].inprogress == true) {
+            $scope.taskInProgress.push(SharedBackend.listOfHowTo[i]);
+        }
     }
-    $window.location.href = '#/overview';
-  };
+
+
+    $scope.goOverview = function(selectedId) {
+
+        for (var i = 0; i < SharedBackend.listOfHowTo.length; i++) {
+            if (selectedId == SharedBackend.listOfHowTo[i].ID) {
+                SharedBackend.currID = selectedId;
+            }
+        }
+        $window.location.href = '#/overview';
+    };
 }]);
 
 
 
 ctnApp.controller('overviewCtrl', ['$scope', '$window', 'SharedBackend', function($scope, $window, SharedBackend) {
 
-  $scope.editEnabled = false;
+    $scope.editEnabled = false;
 
-  $scope.enableEdit = function() {
-    if($scope.editEnabled) {
-      $scope.editEnabled = false;
-    } else {
-      $scope.editEnabled = true;
+    $scope.enableEdit = function() {
+        if ($scope.editEnabled) {
+            $scope.editEnabled = false;
+        } else {
+            $scope.editEnabled = true;
+        }
     }
-  }
 
-$scope.restartSteps = function() {
-  for(var i=0; i<$scope.listOfTasks.length; i++) {
-    $scope.listOfTasks[i].isDone = false;
-  }
-}
+    $scope.restartSteps = function() {
+        for (var i = 0; i < $scope.listOfTasks.length; i++) {
+            $scope.listOfTasks[i].isDone = false;
+        }
+    }
 
-for(var i=0; i<SharedBackend.listOfHowTo.length; i++) {
-  if(SharedBackend.currID == SharedBackend.listOfHowTo[i].ID) {
-    $scope.listOfTasks = SharedBackend.listOfHowTo[i].listOfTasks;
-    $scope.title = SharedBackend.listOfHowTo[i].title;
-  }
-}
+    for (var i = 0; i < SharedBackend.listOfHowTo.length; i++) {
+        if (SharedBackend.currID == SharedBackend.listOfHowTo[i].ID) {
+            $scope.listOfTasks = SharedBackend.listOfHowTo[i].listOfTasks;
+            $scope.title = SharedBackend.listOfHowTo[i].title;
+        }
+    }
 
-  $scope.start = function() {
-    $window.location.href = "#/step";
-  }
+    $scope.start = function() {
+
+        for (var i = 0; i < SharedBackend.listOfHowTo.length; i++) {
+            if (SharedBackend.currID == SharedBackend.listOfHowTo[i].ID) {
+                SharedBackend.listOfHowTo[i].inprogress = true;
+            }
+        }
+        $window.location.href = "#/step";
+    }
 }]);
--- js/service.js
+++ js/service.js
@@ -44,12 +44,12 @@ BackendService.factory('SharedBackend', function() {
                 title: "1. Choose vehicle",
                 desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem I",
                 isDone: false,
-                isCurrent: false
+                isCurrent: true
             }, {
                 title: "2. Get a license plate",
                 desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem I",
                 isDone: false,
-                isCurrent: true
+                isCurrent: false
             }, {
                 title: "3. Get and insurance",
                 desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem I",
@@ -65,7 +65,7 @@ BackendService.factory('SharedBackend', function() {
             inprogress: false,
             isRecommended: false
         }, {
-            title: "Marry a woman from outside Switzerland",
+            title: "Marry a spouse from outside Switzerland",
             ID: 5,
             inprogress: false,
             isRecommended: true
